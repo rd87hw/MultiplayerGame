c77b585dba8350d10c73759bb2fbd0ceb693a791
Nick Mckeeman <nm125@hw.ac.uk>
2020-11-13 13:48:54 +0000

    Added basic leaderboard, needs to be finished.

--- client/app.js
+++ client/app.js
@@ -1,1 +1,15 @@
 const socket = io()
+
+// Starts a function so we can toggle the leaderboard
+function toggleLeaderboard() {
+    // Assign x to the container element, so code is easier to read
+    let x = document.getElementById("container");
+    // If the display style of container is set to none then set it to be display type block
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    }
+    // If its not set to none, then set it to be none
+    else {
+        x.style.display = 'none';
+    }
+}
